5b4678b8695d35682e736ff312b07b787bdf2e4e
Suyash Joshi <suyash.joshi@Suyashs-MBP.attlocal.net>
2022-03-28 16:06:48 -0700

    Updated code and .gitignore file

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
+const RC = require("@ringcentral/sdk").SDK;
 
-const RC = require('@ringcentral/sdk').SDK
 const rcsdk = new RC({
-    'server':       process.env.RC_SERVER_URL,
-    'clientId':     process.env.RC_CLIENT_ID,
-    'clientSecret': process.env.RC_CLIENT_SECRET
+    "server":       process.env.RC_SERVER_URL,
+    "clientId":     process.env.RC_CLIENT_ID,
+    "clientSecret": process.env.RC_CLIENT_SECRET
 });
 const platform = rcsdk.platform();
 
 // login using JWT
 platform.login({
-    'jwt':  process.env.RC_JWT
+    "jwt":  process.env.RC_JWT
 })
 
 platform.on(platform.events.loginSuccess, function(e){
