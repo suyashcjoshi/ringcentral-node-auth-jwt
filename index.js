61314154a7a54d1abb83182ae42ee6eb74bb092c
Suyash Joshi <suyash.joshi@Suyashs-MBP.attlocal.net>
2022-03-28 15:40:18 -0700

    Updated code with improvements

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 require('dotenv').config();
 
 const RC = require('@ringcentral/sdk').SDK
-var rcsdk = new RC({
+const rcsdk = new RC({
     'server':       process.env.RC_SERVER_URL,
     'clientId':     process.env.RC_CLIENT_ID,
     'clientSecret': process.env.RC_CLIENT_SECRET
 });
-var platform = rcsdk.platform();
+const platform = rcsdk.platform();
 
+// login using JWT
 platform.login({
     'jwt':  process.env.RC_JWT
 })
 
 platform.on(platform.events.loginSuccess, function(e){
     console.log("User logged in successfully");
-    getUserExtensionInfo("/restapi/v1.0/account/~/extension", {})
+    getAccountInfo("/restapi/v1.0/account/~", {})
 });
 
-async function getUserExtensionInfo(endpoint, params) {
+async function getAccountInfo(endpoint, params) {
   try {
-    var resp = await platform.get(endpoint, params)
-    var jsonObj = await resp.json()
-    console.log("Extension Details:", jsonObj);
+    let resp = await platform.get(endpoint, params)
+    let jsonObj = await resp.json()
+    console.log("Account Details:", jsonObj);
   } 
   catch (e) {
     console.log("Error occured: " + e.message)
